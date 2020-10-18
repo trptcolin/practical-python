4a6e4ac35ca7a9130e3813a366ad9da32be1a476
Colin Jones <trptcolin@gmail.com>
2020-10-18 14:21:47 -0500

    Complete 1.7

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,19 +2,34 @@
 #
 # Exercise 1.27
 
-f = open("Work/Data/portfolio.csv", "rt")
-[name_header, shares_header, price_header] = next(f).strip().split(",")
-print(f"{name_header:10s} {shares_header:>10s} {price_header:>10s}")
+import csv
+import sys
 
-total_spend = 0.0
+def portfolio_cost(filename):
+    f = open(filename, "rt")
+    rows = csv.reader(f)
+    [name_header, shares_header, price_header] = next(rows)
+    print(f"{name_header:10s} {shares_header:>10s} {price_header:>10s}")
 
-for line in f:
-    [symbol, shares_str, price_str] = line.strip().split(",")
-    shares = int(shares_str)
-    share_price = float(price_str)
-    total_spend += shares * share_price
-    print(f"{symbol:10s} {shares:10d} {share_price:10.2f}")
+    total_spend = 0.0
 
-print("-------")
-print(f"Total cost ${total_spend:0.2f}")
-f.close()
+    for row in rows:
+        try:
+            [symbol, shares_str, price_str] = row
+            shares = int(shares_str)
+            share_price = float(price_str)
+            total_spend += shares * share_price
+            print(f"{symbol:10s} {shares:10d} {share_price:10.2f}")
+        except ValueError:
+            print(f"WARNING: bad input line {row}")
+
+    f.close()
+    return total_spend
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = "Work/Data/portfolio.csv"
+
+cost = portfolio_cost(filename)
+print(f"Total cost ${cost:0.2f}")
