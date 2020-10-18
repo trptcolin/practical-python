4d1d8b69c082771be060b10b17d1997d4aaa2318
Colin Jones <trptcolin@gmail.com>
2020-10-18 13:02:02 -0500

    Complete section 1.3

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,32 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+months = 0
+
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+
+while principal > 0:
+    updated_owed = principal * (1 + rate / 12)
+
+    this_months_payment = payment
+    if months >= extra_payment_start_month and months < extra_payment_end_month:
+        this_months_payment += extra_payment
+
+    if this_months_payment > updated_owed:
+        this_months_payment = updated_owed
+
+    principal = updated_owed - this_months_payment
+
+    total_paid += payment
+    months += 1
+    print(months, round(total_paid, ndigits=2), round(principal, ndigits=2))
+
+print("Total paid", round(total_paid, ndigits=2), end=" ")
+print("over", months, "months")
