6c499a6b81f5678217f124c0bdad343d2cd6ed4e
Colin Jones <trptcolin@gmail.com>
2020-10-18 12:47:33 -0500

    Complete section 1.2

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,15 @@
 # bounce.py
 #
 # Exercise 1.5
+
+
+def count_bounces(n):
+    bounce_count = 0
+    height = 100
+    while bounce_count < n:
+        height *= 0.6
+        bounce_count += 1
+        print(bounce_count, round(height, ndigits=4))
+
+
+count_bounces(10)
